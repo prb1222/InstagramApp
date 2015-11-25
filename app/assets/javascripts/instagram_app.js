4ec2fe3aee76772e5878e0810032c58766de9d49
Peter Benavides <peter.r.benavides@gmail.com>
2015-11-24 23:17:21 -0800

    Added backbone.js

---
+++ app/assets/javascripts/instagram_app.js
@@ -0,0 +1,13 @@
+window.InstagramApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  InstagramApp.initialize();
+});
